0e0cbd069d40ea71ce9ec417485a64033fb8501b
Marcin Puc <marcin.e.puc@gmail.com>
2019-04-09 19:44:50 +0200

    Refactor dicethrow.sh

--- bash/dicethrow.sh
+++ bash/dicethrow.sh
@@ -4,47 +4,44 @@
 function usage {
     cat <<EOF
 Usage:
-	`basename $0` [-h | --help] [<T>d<S>]
-	
+    $(basename "$0") [-h | --help] [<T>d<S>]
+
 	where
         T - number of throws
         S - number of die sides
 EOF
 }
 
-if [ $# -gt 1 ]
-then
-    usage;
-    exit 1;
+if [[ $# -gt 1 ]]; then
+    usage
+    exit 1
 fi
 
 # default is one throw of a standard die
 THROWS=1
 SIDES=6
 
-if [ $# -eq 1 ]
-then
-    if [ $1 = "-h" ] || [ $1 = "--help" ]
-    then
-        usage;
-        exit 0;
+if [[ $# -eq 1 ]]; then
+    if [[ $1 = "-h" || $1 = "--help" ]]; then
+        usage
+        exit 0
     fi
 
-    CHECK=`echo $1 | grep -Ecx '[1-9]([[:digit:]])*d[1-9]([[:digit:]])*'`
+    CHECK="$(echo "$1" | grep -Ecx '[1-9]([[:digit:]])*d[1-9]([[:digit:]])*')"
 
-    if [ $CHECK -ne 1 ]
-    then
-        usage;
-        exit 1;
+    if [[ $CHECK -ne 1 ]]; then
+        usage
+        exit 1
     fi
 
-    PARSED=`echo $1 | grep -Eo '[[:digit:]]+'`
+    PARSED="$(echo "$1" | grep -Eo '[[:digit:]]+')"
 
-    read THROWS SIDES < <(echo $PARSED)
+    readarray -t LINES <<<"$PARSED"
+    THROWS="${LINES[0]}"
+    SIDES="${LINES[1]}"
 fi
 
-for i in `seq $THROWS`;
-do
-    echo $(( $RANDOM % $SIDES + 1 ));
+for _ in $(seq $THROWS); do
+    echo "$((RANDOM % SIDES + 1))"
 done
 
